9279603bbfa3e4bbf19acc6ef6e4c5dc83d44650
BeNeNuTs <david13.tcm@hotmail.fr>
2015-11-12 14:46:02 +0100

    Initialisation du projet
    - Génération d'un terrain en fonction d'une heithtmap

---
+++ Terrain/Texture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Texture {
+
+	public Texture2D texture;
+	[Range(0,100)]
+	public int minHeight;
+	[Range(0,100)]
+	public int maxHeight;
+}
