2a83d1a91d42b222559be9c401b5dc65d47de964
BeNeNuTs <david13.tcm@hotmail.fr>
2015-11-12 14:43:43 +0100

    Initialisation du projet

---
+++ Controller/HUDController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HUDController : MonoBehaviour {
+
+	public TerrainGenerator tGenerator;
+	public Sprite[] Sheightmaps;
+	public Texture2D[] Theightmaps;
+	private int index;
+
+	private Canvas canvas;
+	private Image img;
+
+	private bool isShow;
+
+	void Start(){
+		index = 0;
+		isShow = false;
+
+		canvas = GetComponent<Canvas>();
+		img = GetComponentInChildren<Image>();
+
+		UpdateImg();
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.H)){
+			ToggleHUD();
+		}else if(isShow && Input.GetKeyDown(KeyCode.LeftArrow)){
+			index--;
+			if(index < 0){
+				index = Sheightmaps.Length - 1;
+			}
+
+			UpdateImg();
+		}else if(isShow && Input.GetKeyDown(KeyCode.RightArrow)){
+			index++;
+			if(index > Sheightmaps.Length - 1){
+				index = 0;
+			}
+			
+			UpdateImg();
+		}
+	}
+
+	IEnumerator Coroutine_ToggleHUD(){
+		float newFill = 0f;
+		if(!isShow){
+			newFill = 1f;
+		}
+		float oldFill = img.fillAmount;
+		
+		float fill = img.fillAmount;
+		
+		float time = 1f;
+		float elapsedTime = 0f;
+		while (elapsedTime < time) {
+			fill = Mathf.Lerp(oldFill, newFill, elapsedTime / time);
+			img.fillAmount = fill;
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+
+		img.fillAmount = newFill;
+
+		ToggleHUD();
+	}
+
+	void ToggleHUD(){
+		canvas.enabled = !isShow;
+		isShow = !isShow;
+	}
+
+	void UpdateImg(){
+		img.sprite = Sheightmaps[index];
+		tGenerator.hMap = Theightmaps[index];
+	}
+}
---
+++ Controller/Pod/PodController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PodController : Controller {
+	
+	public override IEnumerator UpdatePosition(float newYPos){
+		StartCoroutine(base.UpdatePosition(newYPos));
+		yield return null;
+	}
+}
